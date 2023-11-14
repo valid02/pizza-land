9ca3bf7177539ca12baff37b11ab6492885b0cfe
Valid Rabiee <validrabieejs@gmail.com>
2023-11-15 03:11:29 +0330

    complating separate  &  toFarsiNumber function

--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -1,15 +1,37 @@
 import classes from './MenuItem.module.css';
 
-const MenuItem = (props) => {
+const MenuItem = props => {
+  const toFarsiNumber = n => {
+    const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
+    return n.toString().replace(/\d/g, x => farsiDigits[x]);
+  };
+
+  function separate(number) {
+    let numStr = String(number).replace(',', '');
+  
+    const [integerPart, decimalPart] = numStr.split('.');
+    
+    const decimal = decimalPart ? `.${decimalPart}` : '';
+  
+    const rgx = /(\d+)(\d{3})/;
+    let formattedIntegerPart = integerPart;
+    while (rgx.test(formattedIntegerPart)) {
+      formattedIntegerPart = formattedIntegerPart.replace(rgx, (match, group1, group2) => `${group1},${group2}`);
+    }
+  
+    return formattedIntegerPart + decimal;
+  }
+  
   return (
     <li className={classes['menu-item']}>
       <div>
         <h3>{props.name}</h3>
         <p>{props.description}</p>
-        <span className={classes.price}>{props.price}</span>
+        <span className={classes.price}>{toFarsiNumber(separate(props.price))} تومان</span>
       </div>
     </li>
   );
-}
+};
  
 export default MenuItem;
