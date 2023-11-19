bb99fdf0f6328c390695aa14f10e309dcfc5223f
Valid Rabiee <validrabieejs@gmail.com>
2023-11-19 19:54:31 +0330

    adding cart to header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import classes from './Header.module.css';
 import pizzasImage from '../../assets/pizza.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = () => {
   return (
     <>
       <header className={classes.header}>
         <h1>پیتزالند</h1>
-        <button>سبد خرید</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={pizzasImage} alt="پیتزا" />
