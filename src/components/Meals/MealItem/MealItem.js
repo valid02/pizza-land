cfca2c447d02e1c59eed57c3b8c76e208313a206
Valid Rabiee <validrabieejs@gmail.com>
2023-11-22 17:02:07 +0330

    adding MealItem component

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,37 @@
+import classes from './MealItem.module.css';
+
+const MealItem = props => {
+  const toFarsiNumber = n => {
+    const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+
+    return n.toString().replace(/\d/g, x => farsiDigits[x]);
+  };
+
+  function separate(number) {
+    let numStr = String(number).replace(',', '');
+  
+    const [integerPart, decimalPart] = numStr.split('.');
+    
+    const decimal = decimalPart ? `.${decimalPart}` : '';
+  
+    const rgx = /(\d+)(\d{3})/;
+    let formattedIntegerPart = integerPart;
+    while (rgx.test(formattedIntegerPart)) {
+      formattedIntegerPart = formattedIntegerPart.replace(rgx, (match, group1, group2) => `${group1},${group2}`);
+    }
+  
+    return formattedIntegerPart + decimal;
+  }
+  
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.description}</p>
+        <span className={classes.price}>{toFarsiNumber(separate(props.price))} تومان</span>
+      </div>
+    </li>
+  );
+};
+ 
+export default MealItem;
