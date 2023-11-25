050fa0b716a76a55a651a2219a748726557c91a8
Valid Rabiee <validrabieejs@gmail.com>
2023-11-25 15:23:51 +0330

    adding CartItem component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,6 +3,7 @@ import { separateNumber, toFarsiNumber } from '../Helpers/Helpers';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 const Cart = props => {
   const cartCtx = useContext(CartContext);
@@ -10,10 +11,25 @@ const Cart = props => {
   const totalAmount = toFarsiNumber(separateNumber(cartCtx.totalAmount));
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = id => {
+    // cartCtx.removeItem(id);
+  }
+
+  const cartItemAddHandler = item => {
+    // cartCtx.addItem({ ...item, amount: 1 });
+  }
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id} 
+          name={item.name} 
+          amount={item.amount} 
+          price={item.price} 
+          onRemove={cartItemRemoveHandler.bind(null, item.id)} 
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,23 @@
+import { separateNumber, toFarsiNumber } from '../Helpers/Helpers';
+import classes from './CartItem.module.css';
+
+const CartItem = props => {
+
+  return (
+    <li className={classes['cart-item']}>
+      <div>
+        <h2>{props.name}</h2>
+        <div className={classes.summary}>
+          <span className={classes.price}>{toFarsiNumber(separateNumber(props.price))} تومان</span>
+          <span className={classes.amount}>x {props.amount}</span>
+        </div>
+      </div>
+      <div className={classes.actions}>
+        <button onClick={props.onRemove}>−</button>
+        <button onClick={props.onAdd}>+</button>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
