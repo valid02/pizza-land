d0eb7596d34ccdf5593c79265e8d23984c2185e3
Valid Rabiee <validrabieejs@gmail.com>
2024-08-03 19:12:21 +0330

    fetch meals data from server

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,49 +1,75 @@
+import { useCallback, useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'پیتزا پپرونی',
-    description: 'خمیر مخصوص چاودار، پنیر موزارلا، پنیر گودا، پپرونی، فلفل تند',
-    price: '261000',
-  },
-  {
-    id: 'm2',
-    name: 'پیتزا استیک مرغ',
-    description: 'خمیر مخصوص چاودار، پنیر موزارلا، پنیر گودا، استیک مرغ، قارچ، فلفل دلمه ای',
-    price: '283500',
-  },
-  {
-    id: 'm3',
-    name: 'پیتزا استیک گوشت',
-    description: 'خمیر مخصوص چاودار، پنیر موزارلا، پنیر گودا، ۱۷۰ گرم راسته گوساله گریل شده، قارچ، زیتون',
-    price: '378000',
-  },
-  {
-    id: 'm4',
-    name: 'پیتزا رست بیف',
-    description: 'خمیر مخصوص چاودار، پنیر موزارلا، پنیر گودا، ۳۰۰ گرم سردست گوساله رست شده و قارچ، زیتون',
-    price: '261000',
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => (
-    <MealItem
-      id={meal.id}
-      key={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fechMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch('https://react-http-ad85d-default-rtdb.firebaseio.com/meals.json');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          description: data[key].description,
+          name: data[key].name,
+          price: data[key].price
+        });
+      }
+
+      setMeals(loadedMeals);
+
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fechMoviesHandler();
+  }, [fechMoviesHandler]);
+
+  let content = <p>هیچ وعده غذایی یافت نشد.</p>;
+
+  if (meals.length > 0) {
+    const mealsList = meals.map(meal => (
+      <MealItem
+        id={meal.id}
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    ));
+    content = <ul>{mealsList}</ul>;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>در حال بارگیری...</p>;
+  }
 
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        {content}
       </Card>
     </section>
   );
